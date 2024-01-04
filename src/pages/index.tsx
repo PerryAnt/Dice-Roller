e30949e06c858f2dfd4b57c13586863ac9209d37
PerryAnt <antonelliperry@gmail.com>
2024-01-04 10:08:38 -0600

    Really fixed page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Perry\'s Dice Roller</title>
+        <title>Perry&apos;s Dice Roller</title>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
         {!user.isSignedIn && <SignInButton />}
